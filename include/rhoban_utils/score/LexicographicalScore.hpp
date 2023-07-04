db48165960cc9b01a5ce665dd9631f699736b237
Grégoire Passault <g.passault@gmail.com>
2023-07-04 17:04:34 +0200

    Adding LexicographicalScore

---
+++ include/rhoban_utils/score/LexicographicalScore.hpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <vector>
+
+namespace rhoban_strategy
+{
+template <typename T>
+class LexicographicalScore
+{
+public:
+  LexicographicalScore() : score()
+  {
+  }
+
+  LexicographicalScore(T value) : score({ value })
+  {
+  }
+
+  void operator=(T value)
+  {
+    score = { value };
+  }
+
+  T& operator[](size_t index)
+  {
+    if (score.size() < index + 1)
+    {
+      score.resize(index + 1);
+    }
+
+    return score[index];
+  }
+
+  bool operator<(const LexicographicalScore<T>& other) const
+  {
+    return std::lexicographical_compare(score.begin(), score.end(), other.score.begin(), other.score.end());
+  }
+
+  operator T() const
+  {
+    return score[0];
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const LexicographicalScore<T>& obj)
+  {
+    for (size_t i = 0; i < obj.score.size(); i++)
+    {
+      os << obj.score[i];
+      if (i < obj.score.size() - 1)
+        os << ", ";
+    }
+
+    return os;
+  }
+
+  std::vector<T> score;
+};
+}  // namespace rhoban_strategy
